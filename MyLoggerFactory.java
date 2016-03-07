12fd8f55cabbbf6b161dc6445a9cab9543f06dff
NappsTech <napps.tech94@gmail.com>
2016-03-07 23:25:18 +0530

    Added files via upload

---
+++ MyLoggerFactory.java
@@ -0,0 +1,16 @@
+package volatileTest;
+
+import java.util.logging.Logger;
+
+public class MyLoggerFactory {
+	public static Logger getSimplestLogger(String name){
+        Logger logger = Logger.getLogger(name);
+        logger.setUseParentHandlers(false);
+        logger.addHandler(new MyConsoleHandler());
+        return logger;
+    }
+
+    public static Logger getSimplestLogger() {
+        return getSimplestLogger("noname");
+    }
+}
