514cb0072ed87bcfcddf3059f4d38663c6d11399
NappsTech <napps.tech94@gmail.com>
2016-03-07 23:00:24 +0530

    volatile experiments

---
+++ UltraSimpleFormatter.java
@@ -0,0 +1,12 @@
+package volatileTest;
+
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+public class UltraSimpleFormatter extends Formatter{
+
+	 @Override
+	    public String format(LogRecord record) {
+	        return formatMessage(record);
+	    }
+}
